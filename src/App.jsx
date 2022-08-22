44afa32671c20ac12825cfa55eb0a9fa9eab47aa
JonathanBorbor <jonathanbor2000@hotmail.com>
2022-08-22 06:42:14 -0500

    Se agregan componentes para listar gastos

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,6 @@
 import { useState } from 'react'
 import Header from './components/Header'
+import ListadoGastos from './components/ListadoGastos';
 import Modal from './components/Modal';
 import { generarId } from './helpers';
 import iconoNuevoGasto from './img/nuevo-gasto.svg'
@@ -24,6 +25,7 @@ function App() {
 
   const guardarGasto = (gasto) => {
     gasto.id = generarId();
+    gasto.fecha = Date.now();
     setGastos([...gastos, gasto]);
   }
 
@@ -37,9 +39,20 @@ function App() {
       />
 
       {isValidPresupuesto && (
-        <div className='nuevo-gasto'>
-          <img src={iconoNuevoGasto} alt="nuevo gasto" onClick={handleNuevoGasto} />
-        </div>
+        <>
+          <main>
+            <ListadoGastos
+              gastos={gastos}
+            />
+          </main>
+          <div className="nuevo-gasto">
+            <img
+              src={iconoNuevoGasto}
+              alt="icono nuevo gasto"
+              onClick={handleNuevoGasto}
+            />
+          </div>
+        </>
       )}
 
       {modal &&
--- src/helpers/index.js
+++ src/helpers/index.js
@@ -4,4 +4,14 @@ const generarId = () => {
     return random + fecha;
 }
 
-export { generarId };
+const formatearFecha = fecha => {
+    const fechaNueva = new Date(fecha);
+    const opciones = {
+        year: 'numeric',
+        month: 'long',
+        day: '2-digit',
+    }
+    return fechaNueva.toLocaleDateString('es-ES', opciones)
+}
+
+export { generarId, formatearFecha };
