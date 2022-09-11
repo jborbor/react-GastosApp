f01e889bd8da931a3f9d2c2d1de0757a8c904d65
JonathanBorbor <jonathanbor2000@hotmail.com>
2022-09-11 16:48:51 -0500

    main-jby-feat: Se añaden calculos de totales disponibles y gastos

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -3,13 +3,20 @@ import ControlPresupuesto from './ControlPresupuesto'
 import NuevoPresupuesto from './NuevoPresupuesto'
 
 
-const Header = ({ presupuesto, setPresupuesto, isValidPresupuesto, setIsValidPresupuesto }) => {
+const Header = ({
+    gastos,
+    presupuesto,
+    setPresupuesto,
+    isValidPresupuesto,
+    setIsValidPresupuesto
+}) => {
     return (
         <header>
             <h1>Planificador de Gastos</h1>
 
             {isValidPresupuesto ? (
                 <ControlPresupuesto
+                    gastos={gastos}
                     presupuesto={presupuesto}
                 />
             ) : (
