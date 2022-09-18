ba99965de9efbd221c303a3a752ebbce6eaf6fa6
JonathanBorbor <jonathanbor2000@hotmail.com>
2022-09-17 19:28:56 -0500

    main-jby-feat: Se añade grafica y Local Storage

--- src/App.jsx
+++ src/App.jsx
@@ -7,9 +7,14 @@ import iconoNuevoGasto from './img/nuevo-gasto.svg'
 
 function App() {
 
-  const [gastos, setGastos] = useState([]);
+  const [gastos, setGastos] = useState(
+    localStorage.getItem('gastos') ? JSON.parse(localStorage.getItem('gastos')) : []
+  );
 
-  const [presupuesto, setPresupuesto] = useState(0);
+  const [presupuesto, setPresupuesto] = useState(
+    Number(localStorage.getItem('presupuesto')) ?? 0
+  );
+
   const [isValidPresupuesto, setIsValidPresupuesto] = useState(false);
 
   const [modal, setModal] = useState(false);
@@ -26,6 +31,23 @@ function App() {
       }, 300);
     }
   }, [gastoEditar])
+
+
+  useEffect(() => {
+    localStorage.setItem('presupuesto', presupuesto ?? 0)
+  }, [presupuesto])
+
+  useEffect(() => {
+    localStorage.setItem('gastos', JSON.stringify(gastos) ?? [])
+  }, [gastos])
+
+  useEffect(() => {
+    const presupuestoLS = Number(localStorage.getItem('presupuesto')) ?? 0;
+
+    if (presupuestoLS > 0) {
+      setIsValidPresupuesto(true);
+    }
+  }, [])
 
 
   const handleNuevoGasto = () => {
